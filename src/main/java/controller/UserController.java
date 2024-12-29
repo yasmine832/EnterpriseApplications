8aef24f8a2adc9bb49058180cafb4de997c5839c
yasmine832 <yasmine.el.mesaoudi.uasani@student.ehb.be>
2024-12-29 17:13:19 +0100

    usercontroller

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import dto.RegisterDTO;
+import entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "User Management", description = "APIs for user authentication and registration")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Operation(summary = "Register a new user")
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registerUser(@Valid @RequestBody RegisterDTO registerDTO) {
+        return userService.registerUser(registerDTO);
+    }
+
+    @Operation(summary = "Get current user details")
+    @GetMapping("/current")
+    public User getCurrentUser(@AuthenticationPrincipal User user) {
+        return user;
+    }
+}
