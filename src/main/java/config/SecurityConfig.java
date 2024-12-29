34cba8ef624d2be82e2c0354ed092e9cccd327d6
yasmine832 <yasmine.el.mesaoudi.uasani@student.ehb.be>
2024-12-29 23:18:26 +0100

    mysql

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -6,16 +6,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 //https://docs.spring.io/spring-security/reference/features/authentication/password-storage.html
 //howtodoinjava.com/spring-security/spring-security-tutorial
@@ -25,29 +19,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean // in memory storage
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.builder()
-                .username("user")
-                .password("password")
-                .roles("USER").build());
-        return manager;
-    }
-
-
-    // to store and retrieve user data from db, connects db directly tru jdbc    @Bean
-    //    public DataSource dataSource() {
-    //        return DataSourceBuilder.create()
-    //                .url("jdbc:mysql://localhost:3306/dailypuzzle")
-    //                .username("root")
-    //                //.password("")
-    //                .driverClassName("com.mysql.cj.jdbc.Driver")
-    //                .build();
-    //
-    //
-    //    }
-
 
     @Bean // access control
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -56,15 +27,16 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/auth/register").permitAll()
                         .requestMatchers("/api/products").permitAll() // allow all
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Swagger UI access
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated() //restrict any other endpoint
                        // .requestMatchers("/api/**").hasRole("USER") //by role
                         //todo
                 )
                 .formLogin(form -> form
                         //.loginPage("/api/auth/login"))//todo for custom
-                        .defaultSuccessUrl("/hello/", true) //Todoo "/home"
+                        .defaultSuccessUrl("/api/auth/current", true) //Todoo "/home"
                         .failureUrl("/login?error=true")
                 );
 
